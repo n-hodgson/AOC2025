56b75515f0b3dfcc2d3751328f1f225aaa1f4804
Neil Hodgson <n.hodgson@maersk.com>
2025-12-06 11:32:08 +0100

    solution to day 4 part 1

--- day04/day04.py
+++ day04/day04.py
@@ -5,9 +5,37 @@ def get_input(fname):
 
     return [line.strip() for line in lines]
 
-def solve(lines):
+def check_location(grid, x, y, win):
+    xmax = len(grid[0])
+    ymax = len(grid)
+    n_rolls = 0
+    for w in win:
+        xd = x + w[0]
+        yd = y + w[1]
+        if (xd >=0) and (xd < xmax) and (yd >= 0) and (yd < ymax):    
+            if grid[yd][xd] == '@':
+                n_rolls += 1
+                if n_rolls == 4:
+                    return False
 
-    return None
+    return True
+
+
+def solve(grid):
+
+    imax = len(grid[0])
+    jmax = len(grid)
+    nlocations = 0
+    win = [(0, 1), (1, 1), (1, 0), (1, -1), 
+           (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+
+    for i in range(imax):
+        for j in range(jmax):
+            if grid[j][i] == '@':
+                if check_location(grid, i, j, win):
+                    nlocations += 1
+
+    return nlocations
 
 
 if __name__=='__main__':
@@ -20,4 +48,8 @@ if __name__=='__main__':
         print('Error, no file')
         sys.exit(1)
   
-    lines = get_input(fname)
+    grid = get_input(fname)
+
+    ans = solve(grid)
+
+    print(f'The number of accessible locations is {ans}')
